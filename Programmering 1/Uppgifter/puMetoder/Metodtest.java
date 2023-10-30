4f9aecdca7693e53d53c095080c4b0866da2a28c
Johan-Svensson-dev <johan.svensson@gtsmarketing.se>
2023-10-31 00:16:06 +0100

    Uppgift2

--- Programmering 1/Uppgifter/puMetoder/Metodtest.java
+++ Programmering 1/Uppgifter/puMetoder/Metodtest.java
@@ -3,9 +3,16 @@ package puMetoder;
 public class Metodtest {
 
 	public static void main(String[] args) {
-		double tal = -56;
+		//uppgift1
+		double tal = 56;
 		char resultat = Uppgift1.ponev(tal);
 		System.out.println(resultat);
+		
+		//uppgift2
+		String förNamn = "Johan";
+		String efterNamn = "Svensson";
+		String telNummer = "133769";
+		Uppgift2.details(förNamn, efterNamn, telNummer);
 	}
 
 }
---
+++ Programmering 1/Uppgifter/puMetoder/Uppgift2.java
@@ -0,0 +1,11 @@
+package puMetoder;
+
+public class Uppgift2 {
+	public static void details(String förNamn, String efterNamn, String telNummer) {
+		System.out.println("Förnamn: " +förNamn);
+		System.out.println("Efternamn: " +efterNamn);
+		System.out.println("Telefonnummer: " +telNummer);
+
+	} 
+			
+}	
