fb3bcc8e746ee2a17c9f98e86df12aa53bcce726
Johan-Svensson-dev <johan.svensson@gtsmarketing.se>
2023-10-31 00:05:40 +0100

    fix build path

--- Programmering 1/Uppgifter/puMetoder/Metodtest.java
+++ Programmering 1/Uppgifter/puMetoder/Metodtest.java
@@ -3,7 +3,7 @@ package puMetoder;
 public class Metodtest {
 
 	public static void main(String[] args) {
-		double tal = 56;
+		double tal = -56;
 		char resultat = Uppgift1.ponev(tal);
 		System.out.println(resultat);
 	}
