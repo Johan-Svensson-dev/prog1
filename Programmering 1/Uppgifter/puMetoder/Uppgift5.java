765e6b85d0dd51989dacd1b2c45c223388eab175
Johan-Svensson-dev <johan.svensson@gtsmarketing.se>
2023-11-14 14:58:38 +0100

    uppg 5-6

--- Programmering 1/Uppgifter/puMetoder/Uppgift5.java
+++ Programmering 1/Uppgifter/puMetoder/Uppgift5.java
@@ -2,12 +2,15 @@ package puMetoder;
 
 public class Uppgift5 {
 	
-	public static String conv (String bokstav) {
+	public static char conv (char bokstav) {
+		if(Character.isLowerCase(bokstav)) {
+		}
 		return Character.toUpperCase(bokstav);
 	}	
 
 	public static void main(String[] args) {
-		String bokstav = "HejHej";
+		char bokstav = 'h';
+		System.out.println(conv(bokstav));
 	}
 
-}
+}	
---
+++ Programmering 1/Uppgifter/puMetoder/Uppgift6.java
@@ -0,0 +1,20 @@
+package puMetoder;
+
+public class Uppgift6 {
+
+	public static void main(String[] args) {
+		char siffra = '7';
+		System.out.println(decider(siffra));
+		
+	}
+	
+	public static boolean decider (char siffra) {
+		if(Character.isDigit(siffra) ) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+}
