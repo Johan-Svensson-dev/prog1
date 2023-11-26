130da716b120ea48cd3b73fb71836a26cd4d2f9e
Johan <johan@pop-os.localdomain>
2023-11-26 19:41:37 +0100

    inlupp2 start

---
+++ Programmering 1/inluppa/inlupp2/Johan.java
@@ -0,0 +1,20 @@
+package inlupp2;
+
+public class Johan {
+
+	public static void main(String[] args) {
+		int size = 8;
+		
+	}
+
+	//Metoden skapar, fyller och returnerar en char-array som innehåller ett slumpmässig urval av små bokstäver, gemener.
+	public static char[] createchararray(int size) {
+		//storleken på arrayn
+		char[] charArray = new char [size];
+		//variabel som skapar en random gemen.
+		Random gemen = new random();
+		for(int i = 0; i )
+		return null;
+		
+	}
+}
